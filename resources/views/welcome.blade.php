576a2318b07c093a81962bac537c60bdbe746a14
Bill Benon <dushimebillbenon@gmail.com>
2021-06-21 10:35:54 +0200

    commit from Bill

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,8 +6,13 @@
 
         <title>Laravel</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/styles.css') }}">
     </head>
     <body class="antialiased">
         <h1 class='app-name'>WELCOME TO STUDENT MANAGEMENT SYSTEM</h1>
+        <div class="option-links-div">
+            <a href="courses/all" class="btn btn-primary">Courses</a>
+            <a href="students/all" class="btn btn-primary">Students</a>
+        </div>
     </body>
 </html>
